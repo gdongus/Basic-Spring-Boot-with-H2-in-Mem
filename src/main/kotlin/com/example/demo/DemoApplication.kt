65e5b0d45849df6bbd094deb4b5539662643eebf
gd <gdongus@gmail.com>
2023-11-13 12:47:16 +0100

    Add debug sql logging for test profile

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,8 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class DemoApplication
 
 fun main(args: Array<String>) {
